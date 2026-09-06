6df1c30087475506bb845e65460e2a851d7f6633
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-06 21:29:16 +0300

    [CHORE]: disable DataSource autoconfiguration

--- src/main/java/com/pdf2tz/server/Pdf2tzApplication.java
+++ src/main/java/com/pdf2tz/server/Pdf2tzApplication.java
@@ -2,8 +2,9 @@ package com.pdf2tz.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Pdf2tzApplication {
 
     public static void main(String[] args) {
