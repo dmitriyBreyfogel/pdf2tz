082cd46ab9167e5c4da9156e98ae3994bf2711c7
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-06 19:08:54 +0300

    [CHORE]: initial commit

---
+++ src/test/java/com/pdf2tz/server/Pdf2tzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdf2tz.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pdf2tzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
