54f4e041af1620acaf259a3258fee480165de12e
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-07 15:05:56 +0300

    [FEAT]: change PdfTextExtractor to accept MultipartFile with validation

--- src/main/java/com/pdf2tz/backend/application/pdf/PdfTextExtractor.java
+++ src/main/java/com/pdf2tz/backend/application/pdf/PdfTextExtractor.java
@@ -1,7 +1,12 @@
 package com.pdf2tz.backend.application.pdf;
 
 import com.pdf2tz.backend.application.ports.PdfReaderPort;
+import com.pdf2tz.backend.error.AppException;
+import com.pdf2tz.backend.error.ErrorCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @Service
 public class PdfTextExtractor {
@@ -11,7 +16,27 @@ public class PdfTextExtractor {
         this.pdfReaderPort = pdfReaderPort;
     }
 
-    public String extract(byte[] content) {
+    public String extract(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw AppException.build(
+                    ErrorCode.FILE_EMPTY,
+                    "Файл пуст"
+            );
+        }
+
+        byte[] content;
+
+        try {
+            content = file.getBytes();
+        }
+        catch (Exception e) {
+            throw AppException.build(
+                    ErrorCode.PDF_READ_ERROR,
+                    "Не удалось прочитать файл",
+                    Map.of("details", e.getMessage())
+            );
+        }
+
         return pdfReaderPort.read(content);
     }
 }
